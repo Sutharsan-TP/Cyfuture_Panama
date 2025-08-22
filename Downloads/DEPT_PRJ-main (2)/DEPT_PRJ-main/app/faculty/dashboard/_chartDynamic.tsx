a67a4af7dab67e257f67c6609ea07d416d2bf747
Sutharsan <sudhuacademics@gmail.com>
2025-08-22 13:22:49 +0530

    Delete Downloads/DEPT_PRJ-main (2)/DEPT_PRJ-main directory

--- Downloads/DEPT_PRJ-main (2)/DEPT_PRJ-main/app/faculty/dashboard/_chartDynamic.tsx
+++
@@ -1,5 +0,0 @@
-// Chart.js dynamic imports for dashboard analytics
-import dynamic from "next/dynamic";
-export const Line = dynamic(() => import("react-chartjs-2").then(mod => mod.Line), { ssr: false });
-export const Radar = dynamic(() => import("react-chartjs-2").then(mod => mod.Radar), { ssr: false });
-export const Pie = dynamic(() => import("react-chartjs-2").then(mod => mod.Pie), { ssr: false });
